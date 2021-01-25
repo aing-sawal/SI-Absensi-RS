12ccd9c0558d571e9469c4eb22b05e1c8966b264
aing-sawal <77959637+aing-sawal@users.noreply.github.com>
2021-01-25 16:09:52 +0700

    Add files via upload

---
+++ beranda_guest.php
@@ -0,0 +1,38 @@
+<div class="container">
+    <div class="row">
+        <div class ="col-xs-12">
+
+            <div class="alert alert-info cs">
+                Selamat Datang <strong>Guest | Sistem Absensi Karyawan Wira Husada</strong>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <!--colomn kedua-->
+        <div class="col-sm-12 col-xs-12">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h3 class="panel-title">Halaman Akun Tamu Sistem Informasi Absensi Rs WiraHusada Kisaran</h3>
+                </div>
+                <div class="panel-body">
+                     <table id="dtskripsi" class="table table-bordered table-striped table-hover">
+                       <thead>
+                          <p align="center"><img src="img/logo.jpg"></img></p>
+                          <p class="label-title" align="center"><strong>RUMAH SAKIT WIRAHUSADA KISARAN</strong></p>
+                          <p class="col-sm-12 col-xs-12" align="center">
+                            Selamat datang di halaman administrator Sistem Informasi Absensi Karyawan Di Rumah Sakit Wira Husada Kisaran.<br>
+                            Melalui halaman ini dapat dilakukan pengelolaan data absensi dan shift masuk kerja karyawan di Rumah Sakit Wira Husada Kisaran<br><br>
+                            Akses menu Master Data pada bagian atas sistem untuk pengelolaan data absensi dan shift kerja karyawan Rumah Sakit Wira Husada.<br>
+                            Untuk mengelola laporan, dapat dilakukan dengan mengakses menu report pada bagian atas sistem.<br>
+                            Akses menu User untuk mengelola informasi tentang user yang login.<br><br><br>
+
+                          </p>
+
+                         </thead
+
+
+                    </table>
+                </div>
+            </div>
+        </div>
+</div>
---
+++ footer.php
@@ -0,0 +1,12 @@
+<div class="container">
+    <div class="row">
+        <div class="col-xs-12">
+            Copyright &COPY; <?= date('Y')?> Admin |
+            <a href="#">Dokumentasi</a>
+            <a href="#">Data Absen</a>
+            <a href="#">Rumah Sakit</a>
+            <a href="#">Wira Husada</a>
+
+        </div>
+    </div>
+</div>
